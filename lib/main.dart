5517921f09ec55e2ecbb3562ee9dc82ccf29954c
kmtaag <kmtaag@up.edu.ph>
2023-05-25 15:10:27 +0800

    fix: routing on main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,56 +1,8 @@
-// import 'package:flutter/material.dart';
-// import 'package:provider/provider.dart';
-// import '../providers/auth_provider.dart';
-// import 'screens/home_page.dart';
-// import 'screens/user_details.dart';
-// import '../screens/login.dart';
-// import 'package:firebase_core/firebase_core.dart';
-// import 'firebase_options.dart';
-
-// void main() async {
-//   WidgetsFlutterBinding.ensureInitialized();
-//   await Firebase.initializeApp(
-//     options: DefaultFirebaseOptions.currentPlatform,
-//   );
-
-//   runApp(
-//     MultiProvider(
-//       providers: [
-//         ChangeNotifierProvider(create: ((context) => AuthProvider())),
-//       ],
-//       child: MyApp(),
-//     ),
-//   );
-// }
-
-// class MyApp extends StatelessWidget {
-//   const MyApp({super.key});
-
-//   // This widget is the root of your application.
-//   @override
-//   Widget build(BuildContext context) {
-//     return MaterialApp(
-//       title: 'Auth',
-//       initialRoute: '/',
-//       theme: ThemeData(
-//         primarySwatch: Colors.blue,
-//       ),
-//       routes: {
-//         '/': (context) => const HomePage(),
-//         '/login': (context) => const LoginPage(),
-//         '/home': (context) => const LoginPage(),
-//         '/user_details': (context) => const UserDetailsPage(),
-//       },
-//     );
-//   }
-// }
-
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 import '../providers/auth_provider.dart';
 import 'screens/home_page.dart';
 import 'screens/user_details.dart';
-import 'screens/add_entry_page.dart';
 import '../screens/login.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'firebase_options.dart';
@@ -84,7 +36,10 @@ class MyApp extends StatelessWidget {
         primarySwatch: Colors.blue,
       ),
       routes: {
-        '/': (context) => const AddEntryPage(),
+        '/': (context) => const HomePage(),
+        '/login': (context) => const LoginPage(),
+        '/home': (context) => const LoginPage(),
+        '/user_details': (context) => const UserDetailsPage(),
       },
     );
   }
