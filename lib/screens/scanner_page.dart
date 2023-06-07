b0412ba40f8ccc862c716599f8fd7c63a478a945
Jazmin Joy B. Iloso <jbiloso@up.edu.ph>
2023-06-07 22:26:37 +0800

    feat: updated read qr

--- lib/screens/scanner_page.dart
+++ lib/screens/scanner_page.dart
@@ -1,3 +1,6 @@
+import 'dart:html';
+
+import 'package:flutter/material.dart';
 import 'package:flutter/src/widgets/framework.dart';
 import 'package:flutter/src/widgets/placeholder.dart';
 import 'package:flutter/widgets.dart';
@@ -11,39 +14,100 @@ class ScannerPage extends StatefulWidget {
 }
 
 class _ScannerPageState extends State<ScannerPage> {
-  final GlobalKey _globalKey = GlobalKey(); 
-  QRViewController? controller;
-  Barcode? result;
+  // final GlobalKey _globalKey = GlobalKey(); 
+  // QRViewController? controller;
+  // Barcode? result;
 
-  void qr(QRViewController controller ){
-    this.controller = controller;
-    controller.scannedDataStream.listen((event){
-      setState((){
-        result = event;
-      });
-    });
+  // void qr(QRViewController controller ){
+  //   this.controller = controller;
+  //   controller.scannedDataStream.listen((event){
+  //     setState((){
+  //       result = event;
+  //     });
+  //   });
+  // }
+
+  final qrKey = GlobalKey(debugLabel: 'QR');
+  QRViewController? controller;
+  Barcode? barcode;
+
+  @override 
+  void dispose(){
+    controller?.dispose();
+    super.dispose();
   }
+
+  // @override
+  // void reassemble() async {
+  //   super.reassemble();
+  //   if(Platform.isAndroid){
+  //     await controller!.pauseCamera();
+  //   }
+  //   controller!.resumeCamera();
+  // }
+
+
   @override
   Widget build(BuildContext context) {
-    return Container(
-      child:
-       Column(
-         children: [
-          Container(
-          width: 200, 
-          height: 200,
-           child: QRView(
-             key: _globalKey, 
-             onQRViewCreated: qr
-           ),
-         ),
-          Container(
-            child:(result != null) ? Text('${result!.code}'): Text('Scan a code'),
-          ),
-         ]
-       ),
+    // return Container(
+    //   child:
+    //    Column(
+    //      children: [
+    //       Container(
+    //       width: 200, 
+    //       height: 200,
+    //        child: QRView(
+    //          key: _globalKey, 
+    //          onQRViewCreated: qr
+    //        ),
+    //      ),
+    //       Container(
+    //         child:(result != null) ? Text('${result!.code}'): Text('Scan a code'),
+    //       ),
+    //      ]
+    //    ),
 
       
+    // );
+    return Stack(
+      alignment: Alignment.center,
+      children: [
+        buildQrView(context),
+        Positioned(bottom: 10, child: buildResult()),
+      ],
     );
   }
+  Widget buildQrView (BuildContext context)=> QRView(
+    key: qrKey, 
+    onQRViewCreated: onQRViewCreated,
+    overlay: QrScannerOverlayShape(
+      borderColor: Theme.of(context).accentColor,
+      borderWidth: 10,
+      borderRadius: 10,
+      borderLength: 20,
+      cutOutSize: MediaQuery.of(context).size.width*0.5,
+
+    ),
+  );
+  void onQRViewCreated(QRViewController controller){
+    setState(()=> this.controller = controller);
+    controller.scannedDataStream
+      .listen((barcode)=> setState(()=> this.barcode = barcode));
+  }
+  
+  Widget buildResult()  => Container(
+    padding: EdgeInsets.all(12),
+    decoration:BoxDecoration(
+      borderRadius: BorderRadius.circular(8),
+      color: Colors.white24,
+    ),
+    child: Text(
+      barcode != null ? 'Result: ${barcode!.code}': 'Scan a code',
+      maxLines: 3
+    ),
+  );
+    
+  
+
+  
 }
